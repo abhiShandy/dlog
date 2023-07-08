cd11f28b98522c811ff51f2fb60a336c3ca66399
abhishandy <abhishek.sandy@gmail.com>
2023-07-08 11:57:35 -0400

    push v1 to github

--- App.js
+++ App.js
@@ -1,20 +1,74 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { FlatList, StyleSheet, SafeAreaView, Text, View } from "react-native";
+import * as SQLite from "expo-sqlite";
+import React, { useEffect, useState } from "react";
+
+const Log = ({ date, content }) => (
+  <View>
+    <Text>{date}</Text>
+    <Text>{content}</Text>
+  </View>
+);
 
 export default function App() {
+  const db = SQLite.openDatabase("dreamlog.db");
+
+  db.transaction((tx) => {
+    tx.executeSql(
+      "create table if not exists logs (id text primary key not null, date text, content text);"
+    );
+    tx.executeSql(
+      "insert into logs (id, date, content) values ('1', 'July 7, 2023', 'Friends laughing at me');"
+    );
+    tx.executeSql(
+      "insert into logs (id, date, content) values ('2', 'July 8, 2023', 'Monsters celebrating my birthday');"
+    );
+  });
+
+  const [logs, setLogs] = useState([
+    {
+      id: "1",
+      date: "July 8, 2023",
+      content: "Default log",
+    },
+  ]);
+
+  const readLogsFromDatabase = () => {
+    db.transaction((tx) => {
+      tx.executeSql("select * from logs", [], (tx, results) => {
+        var temp = [];
+        for (let i = 0; i < results.rows.length; ++i) {
+          temp.push(results.rows.item(i));
+        }
+        setLogs(temp);
+      });
+    });
+  };
+
+  useEffect(() => {
+    readLogsFromDatabase();
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text>Dream Log</Text>
+
+      <FlatList
+        data={logs}
+        renderItem={({ item }) => (
+          <Log date={item.date} content={item.content} />
+        )}
+        keyExtractor={(item) => item.id}
+      ></FlatList>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    marginLeft: 30,
+    marginTop: 50,
   },
 });
